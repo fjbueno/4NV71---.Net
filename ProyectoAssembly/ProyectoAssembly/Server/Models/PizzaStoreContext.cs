b416d7d25a6ca796ca0415247cfd74744a967026
Francisco Javier Bueno Vasquez <fbuenov1300@gmail.com>
2023-03-16 23:00:43 -0600

    ProyectoAssembly
    ProyectoAssembly

---
+++ ProyectoAssembly/ProyectoAssembly/Server/Models/PizzaStoreContext.cs
@@ -0,0 +1,14 @@
+﻿namespace ProyectoAssembly.Server.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using ProyectoAssembly.Shared;
+    public class PizzaStoreContext : DbContext
+    {
+        public DbSet<PizzaSpecial> Specials { get; set; }
+        public PizzaStoreContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+    }
+}
+
