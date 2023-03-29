8096128f4b8c536ba17300d94ec07a3c74930d0f
Francisco Bueno <fbueno.mrysi18@lania.edu.mx>
2023-03-29 01:38:22 -0600

    Proyecto BlazingPizza
    Proyecto BlazingPizza

---
+++ BlazingPizza/BlazingPizza/Server/Models/PizzaStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Shared;
+
+namespace BlazingPizza.Server.Models
+{
+    public class PizzaStoreContext : DbContext
+    {
+        public DbSet<PizzaSpecial> Specials { get; set; }
+        public PizzaStoreContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+    }
+}
